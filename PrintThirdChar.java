2728f6e4b2c5d1072b3b96c35297babd1ac9d646
Divyas2002 <124021270+Divyas2002@users.noreply.github.com>
2023-03-10 14:57:36 +0000

    Add files via upload

---
+++ PrintThirdChar.java
@@ -0,0 +1,13 @@
+public class PrintThirdChar 
+{
+public static void main(String[] args) 
+{
+String s="Laziness pays of overnight however hardwork pays off througout your life";
+{
+for(int i=2;i<s.length() ;i=i+3)
+{
+System.out.println(s.charAt(i));
+}
+}
+}
+}
